beb06b29454850e65486043db4cdf08a9e1aa2a9
AleksandrRyb <inkognito19931@gmail.com>
2023-03-28 12:54:51 +0300

    dotenv logic added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 class Server {
   #app;
@@ -13,8 +14,8 @@ class Server {
 
   async start() {
     try {
-      this.app.listen(PORT, (port) =>
-        console.log(`Server has running on port ${port}`)
+      this.app.listen(PORT, () =>
+        console.log(`Server has running on port ${PORT}`)
       );
     } catch (error) {
       console.error({ error });
