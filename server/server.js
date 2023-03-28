d0d5f19509e3e846f8dd542ccf6d17a8692707a8
AleksandrRyb <inkognito19931@gmail.com>
2023-03-28 12:46:33 +0300

    Run server logic has  created

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const PORT = 5000;
+
+class Server {
+  #app;
+
+  constructor() {
+    this.app = express();
+  }
+
+  async start() {
+    try {
+      this.app.listen(PORT, (port) =>
+        console.log(`Server has running on port ${port}`)
+      );
+    } catch (error) {
+      console.error({ error });
+    }
+  }
+}
