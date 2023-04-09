65ee6b796ff6fc9e546f2f27f8c07ea89c1a6600
AleksandrRyb <inkognito19931@gmail.com>
2023-04-10 00:25:20 +0300

    have added functions validateAccess and Refresh token

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -35,6 +35,24 @@ class TokenService {
 
     return tokenData;
   }
+
+  async valudateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      return userData;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async valudateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+      return userData;
+    } catch (error) {
+      return null;
+    }
+  }
 }
 
 module.exports = new TokenService();
