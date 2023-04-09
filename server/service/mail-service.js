5254cf77abb5caeaed3a12755e466a80c3852256
AleksandrRyb <inkognito19931@gmail.com>
2023-04-09 13:31:52 +0300

    have added sendActivationMail functionality

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -3,16 +3,29 @@ const nodemailer = require("nodemailer");
 class MailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: "",
-      port: "",
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
       auth: {
-        user: "",
-        pass: "",
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
       },
     });
   }
 
-  async sendActivationMail(to, link) {}
+  async sendActivationMail(to, link) {
+    this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: `Активация аккаунта пользователя ${process.env.API_URL}`,
+      text: "",
+      html: `
+          <div>
+            <h1>Для активации перейдите по ссылке</h1>
+            <a href="${link}">${link}</a>
+          </div>
+        `,
+    });
+  }
 }
 
 module.exports = new MailService();
