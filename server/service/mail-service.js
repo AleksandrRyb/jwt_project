501b7ebb4ed79513dfe040d40956c5495a8135c0
AleksandrRyb <inkognito19931@gmail.com>
2023-04-09 12:43:41 +0300

    have added nodemailer and mail constructor

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,4 +1,17 @@
+const nodemailer = require("nodemailer");
+
 class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "",
+      port: "",
+      auth: {
+        user: "",
+        pass: "",
+      },
+    });
+  }
+
   async sendActivationMail(to, link) {}
 }
 
