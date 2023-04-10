800680385e725a8f012c4d682c9f9078a3d56967
AleksandrRyb <inkognito19931@gmail.com>
2023-04-10 17:10:55 +0300

    have added store logic

---
+++ client/src/store/store.ts
@@ -0,0 +1,59 @@
+import { makeAutoObservable } from "mobx";
+import { IUser } from "../models/IUser";
+import AuthService from "../services/AuthService";
+
+export default class Store {
+  user = {} as IUser;
+  isAuth = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setAuth(bool: boolean) {
+    this.isAuth = bool;
+  }
+
+  setUser(user: IUser) {
+    this.user = user;
+  }
+
+  async login(email: string, password: string) {
+    try {
+      const response = await AuthService.login(email, password);
+      localStorage.setItem("accessToken", response.data.accessToken);
+      this.setAuth(true);
+      this.setUser(response.data.user);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  async registration(email: string, password: string) {
+    try {
+      const response = await AuthService.registration(email, password);
+      localStorage.setItem("accessToken", response.data.accessToken);
+      this.setAuth(true);
+      this.setUser(response.data.user);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  async logout() {
+    try {
+      await AuthService.logout();
+      localStorage.removeItem("accessToken");
+      this.setAuth(false);
+      this.setUser({} as IUser);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+}
