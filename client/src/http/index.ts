61a66a8d3df15f3b15fb84855a1e139ab45e3a36
AleksandrRyb <inkognito19931@gmail.com>
2023-04-10 16:05:08 +0300

    have created request interceptor

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -6,3 +6,12 @@ const $api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
 });
+
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization =
+    "Bearer " + localStorage.getItem("accessToken");
+
+  return config;
+});
+
+export default $api;
